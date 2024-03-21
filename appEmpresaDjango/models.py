efc99ad46e98ff042bff3d0d25a7031ab74a29ad
Garai <Garai.m@opendeusto.es>
2024-03-21 16:14:34 +0100

    Clase 3 - pt 2

--- appEmpresaDjango/models.py
+++ appEmpresaDjango/models.py
@@ -9,11 +9,27 @@ class Departamento(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return f"{self.nombre} ({self.telefono})"
+
+    class Meta:
+        verbose_name_plural = "departamentos"
+        verbose_name = "departamento"
+        ordering = ["-created"]
+
 
 class Habilidad(models.Model):
     nombre = models.CharField(max_length=50)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.nombre
+
+    class Meta:
+        verbose_name_plural = "habilidades"
+        verbose_name = "habilidad"
+        ordering = ["-created"]
 
 
 class Empleado(models.Model):
@@ -24,4 +40,13 @@ class Empleado(models.Model):
     habilidad = models.ManyToManyField(Habilidad)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
     # Estar seguro del on_delete cascade
+
+    def __str__(self):
+        return f"{self.nombre} --- {self.antiguedad}"
+
+    class Meta:
+        verbose_name_plural = "empleados"
+        verbose_name = "empleado"
+        ordering = ["-created"]
