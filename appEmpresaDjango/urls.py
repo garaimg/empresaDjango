40efec2775904adf8116e7d31f79c973cb4c313a
Garai <Garai.m@opendeusto.es>
2024-04-18 12:35:06 +0200

    Clase Final 1

--- appEmpresaDjango/urls.py
+++ appEmpresaDjango/urls.py
@@ -14,15 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import DepartamentoListView, DepartamentoDetailView, EmpleadoListView, EmpleadoDetailView
 
 urlpatterns = [
-    path('/', views.index_departamentos, name="index"),
-    path('/departamentos/<int:departamento_id>', views.show_departamento, name="departamentos_show"),
-    path('/departamentos/<int:departamento_id>/empleados', views.index_empleados, name="empleados_index"),
-    path('/empleados/<int:empleado_id>', views.show_empleado, name="empleados_show"),
+    # path('/',views.index_departamentos, name="index" ),
+    path('/', DepartamentoListView.as_view(), name='index'),
+    # path('/departamentos/<int:departamento_id>',views.show_departamento, name="departamentos_show"),
+    path('/departamentos/<int:pk>', DepartamentoDetailView.as_view(), name='departamentos_show'),
+    # path('/departamentos/<int:departamento_id>/empleados', views.index_empleados, name="empleados_index"),
+    path('/departamentos/<int:departamento_id>/empleados', EmpleadoListView.as_view(), name='empleados_index'),
+    # path('/empleados/<int:empleado_id>', views.show_empleado, name="empleados_show"),
+    path('/empleados/<int:pk>', EmpleadoDetailView.as_view(), name="empleados_show"),
     path('/habilidades/<int:habilidad_id>', views.show_habilidad, name="habilidades_show")
 
 ]
